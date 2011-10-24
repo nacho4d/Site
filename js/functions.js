6694440b05f610f4703dd00d211dc35e886931e6
Guillermo Ignacio Enriquez Gutierrez <nacho4d@mac.com>
2011-10-25 03:04:55 +0900

    Added search field

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
- console.log("logaaaaa");
+ //console.log("logaaaaa");
 //Add a functions to "a" elements
-  $("a").click(function() {
-    alert("Hello world!");
+  //$("a").click(function() {
+  //  alert("Hello world!");
+  //});
+  
+  // make the search bar the first responder
+  var searchField = $('#search-field');
+  searchField.focus(function() {
+    searchField.addClass('focused-box');
+  });
+  searchField.blur(function(){
+    searchField.removeClass('focused-box');
   });
   
 });
